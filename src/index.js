351a3983b1ed3d6fb4815a57e5ad641f705b87d9
Kalin Jack <kalinj1997@gmail.com>
2021-03-22 15:01:51 -0400

    First Commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 console.log('%c HI', 'color: firebrick')
+
+let onpageload = document.querySelector("onpage")
+
+document.addEventListener('DOMContentLoaded',function (){
+    dogimages();
+    dogbreeds();
+})
+
+function dogimages(){
+const imgURL = "https://dog.ceo/api/breeds/image/random/4" 
+fetch(imgURL)
+.then(res => res.json())
+.then(function(dogimages) {
+console.log (dogimages)
+});
+}
+
+function dogbreeds(){
+const breedURL = 'https://dog.ceo/api/breeds/list/all'
+fetch(breedURL)
+.then(res => res.json())
+.then(function(dogbreeds) {
+    console.log (dogbreeds)
+});
+}
